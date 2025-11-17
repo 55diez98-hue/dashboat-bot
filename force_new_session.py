3d275055721a4f6616676ce92aa471e8d755de8d
55diez98-hue <55diez98@gmail.com>
2025-11-17 09:39:44 +0400

    Delete force_new_session.py

--- force_new_session.py
+++
@@ -1,7 +0,0 @@
-import os
-session_file = 'monitor_session.session'
-if os.path.exists(session_file):
-    os.remove(session_file)
-    print(f"[FORCE] {session_file} УДАЛЕНА!")
-else:
-    print(f"[FORCE] {session_file} не найдена — можно авторизоваться.")
